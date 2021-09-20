8f7bfa9eed4701b2ff3ca8da7d953f4299be3c46
giabbo79 <g.boccia@almaviva.it>
2021-09-20 15:41:12 +0200

    udpdate advert listener

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,7 +3,8 @@ require 'resolv'
 Chef::Recipe.send(:include, Hops::Helpers)
 
 my_ip = my_private_ip()
-my_gateway_ip = my_gateway_ip()
+#my_gateway_ip = my_gateway_ip()
+my_gateway_ip =  my_private_ip()
 
 #
 # Get all the hostsnames for all hosts in the cluster
